c9799875ffa452f971fd6eeb514f7146424a469d
alex.zhou <33326664@qq.com>
2017-03-26 21:04:26 +0800

    trace reponse_time

--- panda_response.h
+++ panda_response.h
@@ -4,6 +4,7 @@
 #define PANDA_NODE_RESPONSE "response"
 #define PANDA_NODE_RESPONSE_HTTP_STATUS "http_status"
 #define PANDA_NODE_RESPONSE_HTTP_MIMETYPE "http_mimetype"
+#define PANDA_NODE_RESPONSE_RESPONSE_TIME  "response_time"
 
 int panda_response_compose_node(TSRMLS_D);
 int panda_response_init_globals(TSRMLS_D);
